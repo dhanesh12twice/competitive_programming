058a2e9b147cf999c413b01ce65e72b1bd2144b2
dhanesh12twice <75358658+dhanesh12twice@users.noreply.github.com>
2021-08-08 18:26:42 +0530

    tricky problem

---
+++ trailingzerosoffactorial_leetcode.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+   int n = 300;
+   cout << " Trailing zeros for the factorial of " << n << "  = " << (n/5 + n/25 + n/125 + n/625);
+ return 0;
+}
